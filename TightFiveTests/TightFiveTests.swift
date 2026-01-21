3732509f0976aedc0f1f25c31ae90cba8fddd5ac
Jesse Cherry <jessecherrycomedy@gmail.com>
2026-01-20 16:16:44 -0800

    Initial Commit

---
+++ TightFiveTests/TightFiveTests.swift
@@ -0,0 +1,16 @@
+//
+//  TightFiveTests.swift
+//  TightFiveTests
+//
+//  Created by Jesse Cherry on 1/20/26.
+//
+
+import Testing
+
+struct TightFiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
